d73d74fa4235569ecfc615808d4f8ee735d98eb2
glassedgamer <glassedgamer05@gmail.com>
2025-10-23 23:58:18 -0400

    Game is done!!!!!
    WOOOOO

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -4,14 +4,31 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
 
+    [SerializeField] GameObject howToPlay;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        if (howToPlay != null)
+        {
+            howToPlay.SetActive(false);
+        }
     }
     public void MainLevel()
     {
         SceneManager.LoadScene("MainLevel");
+    }
+
+    public void HowToPlay()
+    {
+        howToPlay.SetActive(true);
+    }
+
+    public void HowToPlayBack()
+    {
+        howToPlay.SetActive(false);
     }
 
     public void Quit()
